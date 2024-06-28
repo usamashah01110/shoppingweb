eea6a0e8aab3183adfd0ed3df47d770db7508252
mubi <mubihussain.te@gmail.com>
2024-06-28 18:50:39 +0500

    added login file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,11 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
         <!-- Scripts -->
         @routes
@@ -18,4 +23,11 @@
     <body class="font-sans antialiased">
         @inertia
     </body>
+
+
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <script src="{{asset('js/js/popper.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/main.js')}}"></script>
+
 </html>
